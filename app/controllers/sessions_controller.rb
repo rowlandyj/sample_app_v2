7532d43612cc777663041cbd513310c2079d22a4
Rowland J <rowlandyj@gmail.com>
2013-02-19 19:49:41 -0800

    Finish user edit, update, index, and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by_email(params[:email].downcase)
     if user && user.authenticate(params[:password])
-      session[:user] = user.id 
+      session[:user]=user.id
       sign_in user
-      redirect_to user
+      redirect_back_or user
     else
       flash.now[:error] = 'Invalid email/password combination'
       render 'new'
